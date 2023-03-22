4379979d46e62b6257eaf32524329c20fcb984cb
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-03-22 15:20:57 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int idx = num_list.length-1;
+        for(int i : num_list){
+            answer[idx] = i;
+            idx--;
+        }
+        return answer;
+    }
+}
