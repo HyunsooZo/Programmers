7e99bfa40e9d5994b5afe18d32d77bbfa4b2cfae
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-03-27 10:25:39 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 9.21 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        for(int i = 0; i < s.length() ; i++){
+            int cnt =0 ;
+            for(int j = 0 ; j < s.length() ; j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    cnt++;
+                }
+            }
+            if(cnt==1){
+                answer += s.charAt(i);
+            }
+        }
+        String[] sort = answer.split("");
+        Arrays.sort(sort);
+        return Arrays.toString(sort).replaceAll("[\\[\\]\\,\\s]","");
+    }
+}
