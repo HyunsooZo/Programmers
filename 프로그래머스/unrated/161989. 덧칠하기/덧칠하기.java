55a03219fc08098c004bce27e8b86c29b13d9e21
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-04-19 00:33:10 +0900

    [unrated] Title: 덧칠하기, Time: 0.90 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,33 +1,14 @@
-import java.util.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0 ;
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i : section) list.add(i);
-        
-        int[] temp = new int[n];
-        
-        int j = 0;
-        for(int i = 1 ; i <= n ; i++){
-            if(list.contains(i)){
-                temp[j] = 0;    
-            }else{
-                temp[j] = 1;    
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
             }
-            System.out.print(temp[j]+" ");
-            j++;
         }
-        
-        int i = 0;
-        
-        while(i<temp.length){
-            if(temp[i]==0){
-                answer++;
-                i+=m-1;
-            }
-            i++;
-        }
-        return answer;
+        return cnt;
     }
 }
