4a62483fdbb4fc69fb3ad22b65b4b6e513cfaddc
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-04-25 21:45:01 +0900

    [level 2] Title: 튜플, Time: 0.75 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         List<List<Integer>> tuples = new ArrayList<>();
-        String[] sets = s.substring(2, s.length() - 2).split("\\},\\{");
-        for (String set : sets) {
-            String[] nums = set.split(",");
-            List<Integer> tuple = new ArrayList<>();
+        String[] sArr = s.substring(2, s.length() - 2).split("\\},\\{");
+        for (String str : sArr) {
+            String[] nums = str.split(",");
+            ArrayList<Integer> tuple = new ArrayList<>();
             for (String num : nums) {
                 tuple.add(Integer.parseInt(num));
             }
@@ -14,12 +14,12 @@ class Solution {
         }
 
         Collections.sort(tuples, (a, b) -> a.size() - b.size());
-        Set<Integer> uniqueNums = new HashSet<>();
+        Set<Integer> temp = new HashSet<>();
         int[] answer = new int[tuples.size()];
         int index = 0;
         for (List<Integer> tuple : tuples) {
             for (int num : tuple) {
-                if (uniqueNums.add(num)) {
+                if (temp.add(num)) {
                     answer[index++] = num;
                 }
             }
