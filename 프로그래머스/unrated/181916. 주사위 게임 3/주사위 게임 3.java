98826879579a589f92f6456627c2fc2f3e4444d5
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-25 23:17:36 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] counts = new int[7]; 
+        counts[a]++;
+        counts[b]++;
+        counts[c]++;
+        counts[d]++;
+        
+        boolean isOneOneTwo = false;
+        int maxCount = 0; 
+        for (int count : counts) {
+            maxCount = Math.max(maxCount, count);
+            if(count ==1 ) isOneOneTwo = true;
+        }
+        
+        if (maxCount == 4) {
+            int score = a * 1111;
+            return score;
+        }
+        
+        if (maxCount == 3) {
+            int p = 0 , q = 0;
+            for (int i = 1; i <= 6; i++) {
+                if (counts[i] == 3) {
+                    p = i;
+                } else if (counts[i] == 1) {
+                    q = i;
+                }
+            }     
+            int score = (10 * p + q) * (10 * p + q);
+            return score;
+        }
+        
+        if (maxCount == 2 && !isOneOneTwo) {
+            
+            int p = 0;
+            int q = 0;
+            
+            for (int i = 1; i <= 6; i++) {
+                if (counts[i] == 2) {
+                    if (p == 0) {
+                        p = i;
+                    } else {
+                        q = i;
+                    }
+                }
+            }
+            
+            int score = (p + q) * Math.abs(p - q);
+            
+            return score;
+            
+        }else if(maxCount ==2 && isOneOneTwo){
+            int r = 1 ;
+            
+            for(int i = 1; i<= 6; i++){
+                if(counts[i]==1){
+                    r*=i;
+                }
+            }
+            return r;
+        }
+        
+        int minNum = Integer.MAX_VALUE;
+        for (int i = 1; i <= 6; i++) {
+            if (counts[i] == 1 && i < minNum) {
+                minNum = i;
+            }
+        }
+        
+        return minNum;
+    }
+}
