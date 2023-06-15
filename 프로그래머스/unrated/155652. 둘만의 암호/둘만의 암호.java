b3a0f578c3ca7825cd6ddeb2e59c21bcac17ebae
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-15 19:14:29 +0900

    [unrated] Title: 둘만의 암호, Time: 0.18 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,17 +1,22 @@
 class Solution {
     public String solution(String s, String skip, int index) {
+        
         StringBuilder answer = new StringBuilder();
+        
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int idx = -1;
-        for(int i =0; i<skip.length();i++) {
-            alphabet = alphabet.replace(Character.toString(skip.charAt(i)), "");
+        
+        for(int i = 0; i < skip.length(); i++) {
+            alphabet = alphabet.replace(
+                Character.toString(skip.charAt(i)), "");
         }
-        for(int i =0; i<s.length();i++) {
-            idx=alphabet.indexOf(Character.toString(s.charAt(i)))+index;
+        
+        int idx = -1;
+        
+        for(int i = 0; i < s.length();i++) {
+            idx = alphabet.indexOf(Character.toString(s.charAt(i)))+index;
             answer.append(alphabet.charAt(idx%alphabet.length()));
         }
-
+        
         return answer.toString();
-
     }
 }
