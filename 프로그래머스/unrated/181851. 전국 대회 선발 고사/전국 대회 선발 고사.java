0bdfe220c3c107f7e63a02c424bb2e187c56f779
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-23 01:24:02 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 1.73 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,19 +1,17 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int len = rank.length;
-
         List<int[]> list = new ArrayList<>();
-
-        for (int i = 0; i < len; i++) {
+        
+        int len = rank.length;
+        
+        for (int i = 0; i < len; i++) 
             if (attendance[i]) list.add(new int[]{rank[i], i});
-        }
         
         Collections.sort(list, Comparator.comparingInt(arr -> arr[0]));
         
         return list.get(0)[1] * 10000 
-            + list.get(1)[1] * 100 
-            + list.get(2)[1];
+                + list.get(1)[1] * 100 
+                + list.get(2)[1];
     }
 }
