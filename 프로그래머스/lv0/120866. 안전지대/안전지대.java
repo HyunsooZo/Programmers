a6dff845e91a0eaad31e44fc23c0ebcb2a41a4f4
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-25 03:58:54 +0900

    [level 0] Title: 안전지대, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,77 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            if ((int) Arrays.stream(board[i]).filter(x -> x == 0).count() == 0) {
-                return 0;
-            }
-        }
-        if (board.length == 1) return board[0][0]==1?0:1;
-        int answer = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    if (j == 0) {
-                        board[i][j + 1] = board[i][j + 1] == 1 ? 1 : 2;
-                        if (i == 0) {
-                            board[i + 1][j + 1] = board[i + 1][j + 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                        } else if (i == board.length - 1) {
-                            board[i - 1][j + 1] = board[i - 1][j + 1] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        } else {
-                            board[i + 1][j + 1] = board[i + 1][j + 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                            board[i - 1][j + 1] = board[i - 1][j + 1] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        }
-                    }
-                    if (j == board[i].length - 1) {
-                        board[i][j - 1] = board[i][j - 1] == 1 ? 1 : 2;
-                        if (i == 0) {
-                            board[i + 1][j - 1] = board[i + 1][j - 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                        } else if (i == board.length - 1) {
-                            board[i - 1][j - 1] = board[i - 1][j - 1] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        } else {
-                            board[i + 1][j - 1] = board[i + 1][j - 1] == 1 ? 1 : 2;
-                            board[i - 1][j - 1] = board[i - 1][j - 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        }
-                    }
-                    if (j > 0 && j < board[i].length - 1) {
-                        board[i][j - 1] = board[i][j - 1] == 1 ? 1 : 2;
-                        board[i][j + 1] = board[i][j + 1] == 1 ? 1 : 2;
-                        if (i == 0) {
-                            board[i + 1][j - 1] = board[i + 1][j - 1] == 1 ? 1 : 2;
-                            board[i + 1][j + 1] = board[i + 1][j + 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                        } else if (i == board.length - 1) {
-                            board[i - 1][j - 1] = board[i - 1][j - 1] == 1 ? 1 : 2;
-                            board[i - 1][j + 1] = board[i - 1][j + 1] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        } else {
-                            board[i + 1][j - 1] = board[i + 1][j - 1] == 1 ? 1 : 2;
-                            board[i - 1][j - 1] = board[i - 1][j - 1] == 1 ? 1 : 2;
-                            board[i + 1][j + 1] = board[i + 1][j + 1] == 1 ? 1 : 2;
-                            board[i - 1][j + 1] = board[i - 1][j + 1] == 1 ? 1 : 2;
-                            board[i + 1][j] = board[i + 1][j] == 1 ? 1 : 2;
-                            board[i - 1][j] = board[i - 1][j] == 1 ? 1 : 2;
-                        }
-                    }
+        int answer = 0 , n = board.length , m = n-1;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (i-1 >= 0 && j-1 >= 0 && board[i-1][j-1] == 1||
+                    i-1 >= 0 && board[i-1][j] == 1||
+                    i-1 >= 0 && j+1 <= m && board[i-1][j+1] == 1||
+                    i+1 <= m && j-1 >= 0 && board[i+1][j-1] == 1||
+                    i+1 <= m && board[i+1][j] == 1||
+                    i+1 <= m && j+1 <= m && board[i+1][j+1] == 1||
+                    j+1 <= m && board[i][j+1] == 1||
+                    j-1 >= 0 && board[i][j-1] == 1||
+                   board[i][j] == 1) {
+                    continue; 
                 }
+                answer++;
             }
-        }
-        for (int[] i : board) {
-            System.out.println(Arrays.toString(i));
-        }
-        for (int i = 0; i < board.length; i++) {
-            answer += (int) Arrays.stream(board[i]).filter(x -> x == 0).count();
         }
         return answer;
     }
 }
-                    
