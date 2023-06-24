35a3df1c2d448049b199d64f5ec00129dacd5bba
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-25 04:38:16 +0900

    [unrated] Title: 달리기 경주, Time: 0.07 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -1,21 +1,22 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        HashMap<String,Integer> call = new HashMap<>();
-        for(int i = 0 ; i < players.length ; i++){
-          call.put(players[i],i); 
-        } 
-        for(String s : callings) {
-            int idx = call.get(s);
-            swap(call,players, idx, idx-1);
+        Map<String,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < players.length; i++){
+            map.put(players[i],i);
+        }
+        
+        for(String s : callings){
+            swap(players,map , map.get(s));
         }
         return players;
     }
-     public static void swap(HashMap<String,Integer> call,String[] players,int i ,int j){
-        call.put(players[i],j);
-        call.put(players[j],i);
-        String temp = players[i];
-        players[i] = players[j];
-        players[j] = temp;
+    public void swap(String[] players ,Map<String,Integer> map, int idx){
+        map.put(players[idx] , idx-1);
+        map.put(players[idx-1] , idx);
+        String temp = players[idx-1];
+        players[idx-1] = players[idx];
+        players[idx] = temp;
+        
     }
 }
