d48de3adf2b51e7dd83fc6772b77197fc7aca6e6
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-24 19:55:28 +0900

    [level 0] Title: OX퀴즈, Time: 0.37 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -1,18 +1,28 @@
+import java.util.*;
 class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
         for (int i = 0; i < quiz.length; i++) {
-            String[] temp = quiz[i].split(" ");
-            int x = Integer.parseInt(temp[0]);
-            int y = Integer.parseInt(temp[2]);
-            int z = Integer.parseInt(temp[4]);
-            int result = 0;
-            if (temp[1].charAt(0) == '+') {
-                result = x + y;
-            } else if (temp[1].charAt(0) == '-') {
-                result = x - y;
+            boolean isMinus = false; 
+            if(quiz[i].charAt(0)=='-'){
+                isMinus = true;
+                quiz[i] = quiz[i].substring(1);
             }
-            answer[i] = result == z ? "O" : "X";
+            String[] temp = quiz[i].split(" ");
+            int left1 = Integer.parseInt(temp[0]);
+            int left2 = Integer.parseInt(temp[2]);
+            int right = Integer.parseInt(temp[4]);
+            int cal = 0;
+            if(isMinus) {
+                left1 *= -1;
+            }
+            System.out.println(Arrays.toString(temp));
+            if (temp[1].charAt(0) == '+') {
+                cal = left1 + left2;
+            } else if (temp[1].charAt(0) == '-') {
+                cal = left1 - left2;
+            }
+            answer[i] = cal == right ? "O" : "X";
         }
         return answer;
     }
