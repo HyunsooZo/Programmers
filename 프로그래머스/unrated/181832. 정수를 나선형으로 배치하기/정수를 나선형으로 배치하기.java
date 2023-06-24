006c6e86c10438f18b972aaed560c3b73968455d
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-24 20:02:11 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.07 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        int[] dx ={0,1,0,-1} , dy = {1,0,-1,0};
-        int dir = 0 , row = 0 , col = 0 , num = 1;
+        int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+        int num = 1, dir = 0, r = 0, c = 0;
         
         while (num <= n * n) {
-            answer[row][col] = num++;
-
-            int tempR = row + dx[dir];
-            int tempC = col + dy[dir];
-
+            answer[r][c] = num++;
+            
+            int tempR = r + dx[dir];
+            int tempC = c + dy[dir];
+            
             if (tempR < 0 || tempR >= n 
                 || tempC < 0 || tempC >= n 
                 || answer[tempR][tempC] != 0) {
                 dir = (dir + 1) % 4;
             }
-
-            row += dx[dir];
-            col += dy[dir];
+            
+            r += dx[dir];
+            c += dy[dir];
         }
-
+        
         return answer;
     }
 }
