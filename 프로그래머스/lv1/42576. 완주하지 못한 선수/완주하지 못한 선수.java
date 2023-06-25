36ea20e2f855ca973f28df16652e264b63e7f5bb
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-25 18:01:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.06 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,11 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String,Integer> a = new HashMap<>();
+        for(String s : participant)a.put(s,a.getOrDefault(s,0)+1);
+        for(String s : completion) a.put(s,a.get(s)-1);
         for(String s : participant){
-            a.put(s,a.getOrDefault(s,0)+1);
+            if(a.get(s)>0) return s;  
         }
-        for(String s : completion){
-            a.put(s,a.get(s)-1);
-        }
-        for(String s : participant){
-            if(a.get(s)>0){
-                return s;
-            }
-        }
-        return "";
+        return null;
     }
 }
