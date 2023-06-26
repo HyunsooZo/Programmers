ebd0e9228a872d424b09df40554be90c059d1a85
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-26 17:47:50 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,16 +1,14 @@
-import java.util.*;
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
-        for(int i = 0 ; i < number.length-2 ; i++){
-            for(int j = i+1 ; j < number.length-1 ; j++){
-                for(int k = j+1 ; k < number.length ; k++){
-                     if(number[i]+number[j]+number[k]==0){
+        int answer = 0 , len = number.length;
+        for(int i = 0 ; i < len ; i++){
+            for(int j = i+1 ; j < len ; j++){
+                for(int k = j+1 ; k < len ;k++){
+                    if(number[i]+number[j]+number[k]==0) 
                         answer++;
-                     }
                 }
             }
         }
-        return answer;
+       return answer;
     }
 }
