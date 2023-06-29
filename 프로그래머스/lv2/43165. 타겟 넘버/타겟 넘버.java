ea0f1c5a85d3f3c9ff7a76c29464f211b5ca416c
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-29 19:15:30 +0900

    [level 2] Title: 타겟 넘버, Time: 0.34 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,22 @@
 class Solution {
-    int[] numbers;
+    int[] numbers; 
     int target;
     int answer;
-    
+    public void dfs(int idx , int sum){
+        if(idx==numbers.length){
+            if(sum==target){
+                 answer++;
+            }
+              return;
+        }
+        dfs(idx+1 , sum + numbers[idx]);
+        dfs(idx+1 , sum - numbers[idx]);
+    }
     public int solution(int[] numbers, int target) {
         answer = 0;
-        this.target = target;
         this.numbers = numbers;
+        this.target =target;
         dfs(0,0);
         return answer;
-    }
-    public void dfs(int idx , int sum){
-        if(idx == numbers.length){
-            if(sum==target){
-                answer++;
-            }
-            return;
-        }
-            
-        dfs(idx+1,sum-numbers[idx]);
-        dfs(idx+1,sum+numbers[idx]);
     }
 }
