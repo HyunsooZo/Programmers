b4c4eb984af34e037e41d8791d1bcc8c74fbc8fe
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-29 21:40:38 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.35 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,37 +1,39 @@
 import java.util.*;
 
 class Solution {
-    static int answer=0;
+    static int answer = 0;
+
     public int solution(String s) {
         int len = s.length();
-        if(len%2!=0){ return 0; }
-            
+
         Deque<Character> queue = new LinkedList<>();
         for (int i = 0; i < len; i++) {
             queue.offerLast(s.charAt(i));
         }
+
         int cnt = 0;
         while (cnt < queue.size()) {
             isProper(queue);
             queue.offerLast(queue.pollFirst());
             cnt++;
         }
+
         return answer;
     }
-    public static void isProper(Deque<Character> queue){
+
+    public static void isProper(Deque<Character> queue) {
         Stack<Character> stack = new Stack<>();
         for (char c : queue) {
-            if (c == '(' || c == '[' || c == '{') {
+            if (stack.empty()) {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) {return;}
                 char top = stack.peek();
-                if ((top == '(' && c == ')') || 
-                    (top == '[' && c == ']') || 
-                    (top == '{' && c == '}')) {
+                if (c == '(' || c == '[' || c == '{') {
+                    stack.push(c);
+                } else if ((top == '(' && c == ')') || 
+                           (top == '[' && c == ']') || 
+                           (top == '{' && c == '}')) {
                     stack.pop();
-                } else {
-                    return;
                 }
             }
         }
