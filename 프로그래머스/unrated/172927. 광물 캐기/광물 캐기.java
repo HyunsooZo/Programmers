87b1baf4641f009dc0bc40a71b4923d1953ac04c
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-30 15:30:50 +0900

    [unrated] Title: 광물 캐기, Time: 0.49 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] picks, String[] minerals) {
+        int answer = 0;
+
+        List<int[]> list = new ArrayList<>();
+
+        int gok =picks[0]+picks[1]+picks[2];
+
+        for(int i=0;i<minerals.length;i+=5){
+            if(gok==0) break;
+            int sum=0,diamond=0,iron=0,stone=0;
+            gok--;
+            for(int j=i;j<i+5;j++){
+                if(j==minerals.length){
+                    break;
+                }
+                
+                String type =minerals[j];
+
+                if(type.equals("diamond")){
+                    sum+=25;
+                    diamond++;
+                }else if(type.equals("iron")){
+                    sum+=5;
+                    iron++;
+                }else{
+                    sum+=1;
+                    stone++;
+                }
+            }
+            list.add(new int[]{sum,diamond,iron,stone});
+        }
+
+        Collections.sort(list,(a,b)->(b[0]-a[0]));
+
+        int idx = -1;
+
+        for(int i=0;i<list.size();i++){
+
+            int dia=list.get(i)[1];
+            int iron= list.get(i)[2];
+            int stone= list.get(i)[3];
+
+            if(picks[0]>=1){
+                answer+=dia+iron+stone;
+                picks[0]--;
+            }else if(picks[1]>=1){
+                answer+=dia*5+iron+stone;
+                picks[1]--;
+            }else if(picks[2]>=1){
+                answer+=list.get(i)[0];
+                picks[2]--;
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
