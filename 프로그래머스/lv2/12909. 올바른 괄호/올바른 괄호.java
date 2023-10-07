54b06ab481a221951c4f0e7278158ed8727c65b8
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-10-08 00:50:33 +0900

    [level 2] Title: 올바른 괄호, Time: 17.02 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,19 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character> stack = new Stack<>();
-        if(s.charAt(0)==')'){
-            return false;
-        }
-        for(int i = 0 ; i < s.length() ; i ++){
-            char elem = s.charAt(i);
-            if(elem=='('){
-                stack.push(elem);    
-            }else if(!stack.isEmpty()&& elem==')'){
-                stack.pop();
-            }else if(stack.isEmpty()){
+        Deque<Character> dq = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                dq.add(c);
+            } else if (c == ')'&&!dq.isEmpty() && dq.peekLast() == '(') {
+                dq.pollLast(); 
+            } else {
                 return false;
             }
         }
-        if(!stack.isEmpty()){
-            return false;
-        }
-        return true;
+
+        return dq.isEmpty(); 
     }
 }
