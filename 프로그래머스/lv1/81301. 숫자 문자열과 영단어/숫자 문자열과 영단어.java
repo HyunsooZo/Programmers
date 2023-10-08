c79f7da27ea26c5e4dd00ae3e7b4376695fa8490
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-10-08 16:39:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.54 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,10 +1,19 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        String[] str = {"zero","one","two",
-                        "three","four","five",
-                        "six","seven","eight","nine"};
-        for(int i = 0 ; i < str.length ; i++){
-            s = s.replaceAll(str[i],String.valueOf(i));
+       Map<String,String> a = new HashMap<>();
+            a.put("zero","0");
+            a.put("one","1");
+            a.put("two","2");
+            a.put("three","3");
+            a.put("four","4");
+            a.put("five","5");
+            a.put("six","6");
+            a.put("seven","7");
+            a.put("eight","8");
+            a.put("nine","9");
+        for(String string : a.keySet()){
+            s = s.replaceAll(string,a.get(string));
         }
         return Integer.parseInt(s);
     }
