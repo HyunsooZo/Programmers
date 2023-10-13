9879b6541521c8fee1145a42c62ee31169bbd20a
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-10-13 21:40:53 +0900

    [level 1] Title: 추억 점수, Time: 11.28 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,17 +1,19 @@
 import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        Map<String, Integer> map = IntStream.range(0, name.length)
-                .boxed()
-                .collect(Collectors.toMap(i -> name[i], i -> yearning[i]));
-
-        return Arrays.stream(photo)
-                .mapToInt(people -> Arrays.stream(people)
-                        .filter(map::containsKey)
-                        .mapToInt(map::get)
-                        .sum())
-                .toArray();
+        Map<String,Integer> map = new HashMap<>();
+        int len = name.length ,len2 = photo.length;
+        for(int i = 0 ; i < len ; i++) map.put(name[i],yearning[i]);
+        
+        int[] answer = new int[len2];
+        
+        for(int i = 0 ; i < len2 ; i++){
+            int temp = 0;
+            for(int j = 0 ; j < photo[i].length ; j++){
+                if(map.containsKey(photo[i][j])) temp+=map.get(photo[i][j]);
+            }
+            answer[i] = temp;
+        }
+        return answer;
     }
 }
