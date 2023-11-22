f9abe947174ee9a0a374a455294e730adba285bd
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-11-22 23:50:46 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 2.64 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/README.md
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/README.md
@@ -1,10 +1,10 @@
-# [unrated] 배열의 원소 삭제하기 - 181844 
+# [level unrated] 배열의 원소 삭제하기 - 181844 
 
 [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181844) 
 
 ### 성능 요약
 
-메모리: 83.7 MB, 시간: 0.09 ms
+메모리: 75.7 MB, 시간: 2.64 ms
 
 ### 구분
 
@@ -12,7 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2023년 11월 3일 23:50:44
 
 ### 문제 설명
 
@@ -70,4 +74,4 @@ Empty
 </ul>
 
 
-> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,22 +1,15 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        List<Integer> list = new ArrayList<>();
-        for(int i : arr){
-            boolean check = false;
-            for(int j : delete_list){
-                if(i==j){
-                    check = true;
+        int[] answer = {};
+        for(int i = 0 ; i < delete_list.length ; i++){
+            for(int j = 0 ;  j < arr.length ; j++){
+                if(delete_list[i]==arr[j]){
+                    arr[j]=0;
                     break;
                 }
             }
-            if(!check) list.add(i);
         }
-        int[] answer = new int[list.size()];
-        int idx=0;
-        for(int i : list){
-            answer[idx++] = i;
-        }
-        return answer;
+        return Arrays.stream(arr).filter(num -> num != 0).toArray();
     }
 }
