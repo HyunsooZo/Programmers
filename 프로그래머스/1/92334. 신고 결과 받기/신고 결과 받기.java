bcb9d8aae4bb0d8f53d817924002c5b3d262a33b
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-11-25 03:10:01 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.09 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,31 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> reporters = new HashMap<>();
-        Map<String, Integer> count = new HashMap<>();
-        Set<String> check = new HashSet<>();
-        int[] answer = new int[id_list.length];
-        
-        for(String id : id_list){
-            reporters.put(id,new HashSet<>());
-        }
-        
-        for(String repo : report){
-            if(check.contains(repo)) continue;
-            String[] temp = repo.split(" ");
-            reporters.get(temp[0]).add(temp[1]);
-            count.put(temp[1],count.getOrDefault(temp[1],0)+1);
-            check.add(repo);
-        }
-        
-        for(int i = 0 ; i < id_list.length ; i++){
-            Set<String> set = reporters.get(id_list[i]);
-            for(String name : set){
-                if(count.get(name) >= k){
-                    answer[i]++;
-                }
-            }
-        }
+        int[] answer = {};
         return answer;
     }
 }
