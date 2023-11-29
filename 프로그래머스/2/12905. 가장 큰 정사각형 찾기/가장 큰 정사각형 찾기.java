ce4a13421d52bb99204c9b3a4cbba5903c4c37ea
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-11-30 01:25:26 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 15.61 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,36 +1,34 @@
-import java.util.*;
 class Solution{
     public int solution(int [][]board){
-        int r = board.length;
-        int c = board[0].length;
+        int answer = 0;
+        int len = board.length;
+        int len2 = board[0].length;
+
+        int[][] dp = new int[len][len2];
         
-        if(r==1 || c==1) return 1;
-        
-        int[][] dp = new int[r][c];
-        //첫열복사
-        dp[0] = Arrays.copyOfRange(board[0],0,c);
-        //첫행복사
-        for(int i = 0 ; i < r ; i++){
+        for(int i = 0 ; i < len ; i++){
+            //첫열복사
             dp[i][0] = board[i][0];
+            answer = Math.max(answer,board[i][0]);
+        
         }
-        //보드 반복 돌면서 만약 1이면? 전열/전행/대각선전 값 확인
-        //최솟값에 1 더해서 현재 크기 (길이)확인. 
-        //쭉쭉 돌면서 다름 대각선에 도달했을때 다시 크기(길이) 확인
-        //1인 맨 끝부분 제곱하면? 사각형크기?
+          
+        for(int i = 0 ; i < len2 ; i++){
+            //첫행복사
+            dp[0][i] = board[0][i];
+            answer = Math.max(answer,board[0][i]);
+        }
         
-        int maxLen = Integer.MIN_VALUE;
         
-        for(int i = 1; i < r ; i++){
-            for(int j = 1; j < c ;j++){
+        for(int i = 1 ; i < len ; i++){
+            for(int j = 1 ; j < len2 ; j++){
+                // 대각선/위/왼쪽 중 최솟값 +1 대입
                 if(board[i][j]==1){
-                    dp[i][j] = Math.min(
-                        dp[i-1][j-1], //대각선전
-                        Math.min(dp[i][j-1],dp[i-1][j]) // 전행/전열
-                    )+1; //일더함
-                    maxLen = Math.max(maxLen,dp[i][j]);
+                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]))+1;
+                    answer = Math.max(answer,dp[i][j]);    
                 }
             }
-        }       
-        return maxLen*maxLen;
+        }
+        return answer*answer;
     }
 }
