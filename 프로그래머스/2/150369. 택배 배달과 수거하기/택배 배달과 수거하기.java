b63be78df999d796e7ee0cd059a688417ccb1af3
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-12-08 17:55:06 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.18 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,21 +2,21 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0, deliver = 0, pickup = 0;
 
-        // 뒤부터 훑으면 반복문 하나로 풀이가능
-        for (int i = n - 1; i >= 0; i--) {
+        // 각집 방문
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            deliver -= deliveries[i];
-            pickup -= pickups[i];
+            deliver -= deliveries[n - 1 - i];
+            pickup -= pickups[n - 1 - i];
 
-            //음수면 카운트(이동횟수) 추가! 캐파시티 만큼 박스리필!
+            // 음수면 카운트(이동횟수) 추가! 캐파시티 만큼 박스리필!
             while (deliver < 0 || pickup < 0) {
                 count++;
                 deliver += cap;
                 pickup += cap;
             }
 
-            //이동횟수 * (2*거리(왕복이니까)) 더함. 
-            answer += count * (2 * (i + 1));
+            // 이동횟수 * (2*거리(왕복이니까)) 더함!
+            answer += count * (2 * (n - i));
         }
         return answer;
     }
