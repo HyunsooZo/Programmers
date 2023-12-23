22fcb062b8715dbf0e0c1651da703018735a7477
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-12-24 00:06:19 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.60 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,13 +1,7 @@
 function solution(arr) {
-    var answer = [];
-    for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] >= 50 && arr[i] % 2 == 0){
-            answer.push(arr[i]/2);
-        }else if(arr[i] < 50 && arr[i] % 2 !=0){
-            answer.push(arr[i]*2);
-        }else{
-            answer.push(arr[i]);
-        }
-    }
-    return answer;
+    return arr.map(c => {
+        if (c >= 50 && c % 2 === 0) return c / 2;
+        else if (c < 50 && c % 2 !== 0) return c * 2;
+        else return c;
+    });
 }
