940863657fd9d65fbae6f1da82013f2c019df057
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2024-01-04 21:17:42 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,9 @@
+SELECT animal_id, name, sex_upon_intake
+from ANIMAL_INS
+where name = 'Lucy'
+    or name = 'Ella' 
+    or name = 'Pickle'
+    or name = 'Rogan'
+    or name = 'Sabrina'
+    or name = 'Mitty';
+    
