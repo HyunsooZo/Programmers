469760aa52463e1e66ceda4bbd38b9872943a2e3
HyunsooZo <bzhs1992@icloud.com>
2025-06-28 14:28:51 +0900

    [level 2] Title: 가장 큰 수, Time: 193.82 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,19 +1,15 @@
 import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
-        if (Arrays.stream(numbers).allMatch(num -> num == 0)) {
-            return "0";
+        var numberStrings = new String[numbers.length];
+        var numberStringIndex = 0;
+        for(var number : numbers){
+            numberStrings[numberStringIndex++] = String.valueOf(number); 
         }
-        String[] answer = Arrays.stream(numbers)
-            .boxed()
-            .sorted((o1,o2)-> {
-                String oo1 = String.valueOf(o1);
-                String oo2 = String.valueOf(o2);
-                return (oo2+oo1).compareTo(oo1+oo2);
-            })
-            .map(Object::toString)
-            .toArray(String[]::new);
         
-        return String.join("",answer);
+        Arrays.sort(numberStrings, (a,b) -> (b+a).compareTo(a+b));
+        
+        return String.join("",numberStrings).replaceAll("^0+", "0");
     }
 }
