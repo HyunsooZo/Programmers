9abc4970c5b15f5bd430be39a460d25d93f5b443
HyunsooZo <bzhs1992@icloud.com>
2025-06-29 15:58:28 +0900

    [level 2] Title: 전화번호 목록, Time: 272.75 ms, Memory: 246 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,6 @@
-import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
-        var prefixSet = new HashSet<String>(Arrays.asList(phone_book));
-        for(var phoneNumber : phone_book){
-            for (var lenth = 1; lenth < phoneNumber.length(); lenth++) {
-                if (prefixSet.contains(phoneNumber.substring(0, lenth))) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        boolean answer = true;
+        return answer;
     }
 }
